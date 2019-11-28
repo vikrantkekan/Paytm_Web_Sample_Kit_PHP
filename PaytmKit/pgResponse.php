8a677ec3ab81ba87e14924726b6ed33cd8350fce
Vikrant Kekan <48706063+vikrantkekan@users.noreply.github.com>
2019-11-28 17:32:55 +0530

    Validation of Transaction Code Included
    To Fetch status of a payment transaction where real time response was not provided (due to system failures, API timeout etc.).

--- PaytmKit/pgResponse.php
+++ PaytmKit/pgResponse.php
@@ -2,7 +2,7 @@
 header("Pragma: no-cache");
 header("Cache-Control: no-cache");
 header("Expires: 0");
-
+session_start();
 // following files need to be included
 require_once("./lib/config_paytm.php");
 require_once("./lib/encdec_paytm.php");
@@ -14,20 +14,33 @@ $isValidChecksum = "FALSE";
 $paramList = $_POST;
 $paytmChecksum = isset($_POST["CHECKSUMHASH"]) ? $_POST["CHECKSUMHASH"] : ""; //Sent by Paytm pg
 
-//Verify all parameters received from Paytm pg to your application. Like MID received from paytm pg is same as your application’s MID, TXN_AMOUNT and ORDER_ID are same as what was sent by you to Paytm PG for initiating transaction etc.
+//Verify all parameters received from Paytm pg to your application. Like MID received from paytm pg is same as your applicationâ€™s MID, TXN_AMOUNT and ORDER_ID are same as what was sent by you to Paytm PG for initiating transaction etc.
 $isValidChecksum = verifychecksum_e($paramList, PAYTM_MERCHANT_KEY, $paytmChecksum); //will return TRUE or FALSE string.
 
 
 if($isValidChecksum == "TRUE") {
+	
+	
+	
 	echo "<b>Checksum matched and following are the transaction details:</b>" . "<br/>";
 	if ($_POST["STATUS"] == "TXN_SUCCESS") {
 		echo "<b>Transaction status is success</b>" . "<br/>";
 		//Process your transaction here as success transaction.
 		//Verify amount & order id received from Payment gateway with your application's order id and amount.
-	}
-	else {
-		echo "<b>Transaction status is failure</b>" . "<br/>";
-	}
+		
+$_SESSION['ORDER_ID']=$_POST["ORDERID"];
+$_SESSION['CUST_ID']='CUST_ID';//$CUST_ID
+$_SESSION['INDUSTRY_TYPE_ID']='Enter Your INDUSTRY_TYPE_ID';
+$_SESSION['CHANNEL_ID']='Enter Your CHANNEL_ID';
+$_SESSION['TXN_AMOUNT']=$_POST["TXNAMOUNT"];
+$_SESSION['WEBSITE']='PAYTM_MERCHANT_WEBSITE';
+$_SESSION['CALLBACK_URL']='http://localhost/PaytmKit/pgResponse.php';
+		
+header('location:validate-transaction.php');	// redirectiing to validate transaction	
+}
+else{
+echo "<b>Transaction status is failure</b>" . "<br/>";
+}
 
 	if (isset($_POST) && count($_POST)>0 )
 	{ 
